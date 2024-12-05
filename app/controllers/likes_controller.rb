4f3d36feb4efc1522bc9af97a2be69d756a91f88
Ximizu <ximizu@hey.com>
2024-12-05 07:26:14 +0100

    Like button with counter done

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,7 +5,7 @@ class LikesController < ApplicationController
   end
 
   def destroy
-    @post = current_user.posts.find_by(id: params[:post_id])
-    current_user.likes.where(post_id: @post.id).delete_all
+    @post = Post.find(params[:post_id])
+    current_user.likes.where(post_id: @post.id, user_id: current_user.id).delete_all
   end
 end
